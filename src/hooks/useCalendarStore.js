e5df6e1c785db22c1fb53c3aaf14bd683744ad7a
Martin Ezequiel Williner <williner.dev@gmail.com>
2024-05-26 18:57:25 -0300

    Calendar Events: create new event w/backend connection

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,26 +1,30 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from '../store';
+import calendarApi from '../api/calendarApi';
 
 
 export const useCalendarStore = () => {
   
     const dispatch = useDispatch();
     const { events, activeEvent } = useSelector( state => state.calendar );
+    const { user } = useSelector( state => state.auth );
 
     const setActiveEvent = ( calendarEvent ) => {
         dispatch( onSetActiveEvent( calendarEvent ) )
     }
 
     const startSavingEvent = async( calendarEvent ) => {
-        // TODO: llegar al backend
 
-        // Todo bien
         if( calendarEvent._id ) {
             // Actualizando
             dispatch( onUpdateEvent({ ...calendarEvent }) );
         } else {
             // Creando
-            dispatch( onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }) );
+
+            const { data } = await calendarApi.post('/events', calendarEvent); // coneccion post al back que tiene ya la db
+            
+
+            dispatch( onAddNewEvent({ ...calendarEvent, id: data.evento.id, user}) );
         }
     }
 
