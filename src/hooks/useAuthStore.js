cd12c7a869542238fca681e53af0a6991a9db94e
Martin Ezequiel Williner <williner.dev@gmail.com>
2024-05-26 20:01:13 -0300

    clear all info from calendar when you logout

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -2,7 +2,7 @@
 
 import { useDispatch, useSelector } from "react-redux"
 import { calendarApi } from "../api";
-import { onChecking, onLogin, onLogout, clearErrorMessage, onRegister } from "../store";
+import { onChecking, onLogin, onLogout, clearErrorMessage, onRegister, onLogoutCalendar } from "../store";
 
 export const useAuthStore = () => {
     const {status, user, errorMessage} = useSelector(state => state.auth)
@@ -81,6 +81,7 @@ export const useAuthStore = () => {
 
     const startLogout = () => {
         localStorage.clear();
+        dispatch(onLogoutCalendar());
         dispatch(onLogout());
     }
 
